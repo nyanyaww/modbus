1c29bce42b8a579a15c0d74f9e5a164c3da2ecfb
lcs <515478826@qq.com>
2019-05-29 00:05:37 +0800

    好吧

--- src/com/lcs/Client/Client.java
+++ src/com/lcs/Client/Client.java
@@ -1,16 +1,28 @@
-// 文件名 client.java
-package com.lcs;
+package com.lcs.Client;
+
 import java.net.*;
 import java.io.*;
 
 public class Client {
-    public static void main(String[] args) {
-        String serverName = "localhost";
-        int port = 8011;
+    private String ip;
+    private int port;
+
+    private Client() {
+        this.ip = "localhost";
+        this.port = 501;
+    }
+
+    void init() {
+
+    }
+
+    private void run() {
         try {
-            System.out.println("连接到主机：" + serverName + " ，端口号：" + port);
-            Socket client = new Socket(serverName, port);
+            System.out.println("连接到主机：" + ip + " ，端口号：" + port);
+            Socket client = new Socket(ip, port);
             System.out.println("远程主机地址：" + client.getRemoteSocketAddress());
+
+
             OutputStream outToServer = client.getOutputStream();
             DataOutputStream out = new DataOutputStream(outToServer);
 
@@ -18,9 +30,17 @@ public class Client {
             InputStream inFromServer = client.getInputStream();
             DataInputStream in = new DataInputStream(inFromServer);
             System.out.println("服务器响应： " + in.readUTF());
+
+
             client.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+
+    public static void main(String[] args) {
+        Client client = new Client();
+        client.run();
     }
 }
