52f469c60595a8b2bf11e6065f582589594d2f13
lcs <515478826@qq.com>
2019-06-02 02:17:48 +0800

    妈的

---
+++ src/com/lcs/protocol/Message/MessageMap.java
@@ -0,0 +1,75 @@
+package com.lcs.protocol.Message;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * @author nyanyaww
+ * @program modbus
+ * @description 清晰的信息类，把信息看成字典
+ * @create 2019-06-02 02:00
+ **/
+public class MessageMap {
+    Map<String, Character> message = new Map<String, Character>() {
+        @Override
+        public int size() {
+            return 0;
+        }
+
+        @Override
+        public boolean isEmpty() {
+            return false;
+        }
+
+        @Override
+        public boolean containsKey(Object key) {
+            return false;
+        }
+
+        @Override
+        public boolean containsValue(Object value) {
+            return false;
+        }
+
+        @Override
+        public Character get(Object key) {
+            return null;
+        }
+
+        @Override
+        public Character put(String key, Character value) {
+            return null;
+        }
+
+        @Override
+        public Character remove(Object key) {
+            return null;
+        }
+
+        @Override
+        public void putAll(Map<? extends String, ? extends Character> m) {
+
+        }
+
+        @Override
+        public void clear() {
+
+        }
+
+        @Override
+        public Set<String> keySet() {
+            return null;
+        }
+
+        @Override
+        public Collection<Character> values() {
+            return null;
+        }
+
+        @Override
+        public Set<Entry<String, Character>> entrySet() {
+            return null;
+        }
+    };
+}
--- src/com/lcs/protocol/Message/MessageParser.java
+++ src/com/lcs/protocol/Message/MessageParser.java
@@ -2,7 +2,10 @@ package com.lcs.protocol.Message;
 
 import com.lcs.Test.HexConvert;
 
-import java.util.Hashtable;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * @author nyanyaww
@@ -16,6 +19,7 @@ public class MessageParser {
     private String functionCode;
     private String startAddr;
     private String dataLength;
+    private Map<String, Character> stringMap = new HashMap<>();
 
     public MessageParser(String request) {
         this.request = request;
@@ -31,22 +35,24 @@ public class MessageParser {
         startAddr = sss[2] + sss[3];
         dataLength = sss[4] + sss[5];
 
-        System.out.println(clientId);
-        System.out.println(functionCode);
-        System.out.println(startAddr);
-        System.out.println(dataLength);
+        char temp = 11;
+        stringMap.put("clientId", temp);
+        System.out.println(stringMap.get("clientId"));
+//        stringMap.put("functionCode", (char) Integer.parseInt(functionCode, 16));
+//        stringMap.put("startAddr", (char) Integer.parseInt(startAddr, 16));
+//        stringMap.put("dataLength", (char) Integer.parseInt(dataLength, 16));
+
     }
 
-
-    public void toasd(){
-        int i = Integer.parseInt(startAddr,16);//16进制
-        System.out.println(Integer.toBinaryString(i));//2进制
+    public Map<String, Character> getStringMap() {
+        return stringMap;
     }
 
     public static void main(String[] args) {
         MessageParser messageParser = new MessageParser("010100130015");
         messageParser.preHandle();
-        messageParser.toasd();
-        System.out.println(Integer.parseInt("1",2));
+        MessageMap messageMap = new MessageMap();
+//        messageMap.message = messageParser.getStringMap();
+        System.out.println(messageParser.getStringMap());
     }
 }
