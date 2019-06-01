209b297e9c16e7309c0cff324c709ab002bf5d4c
lcs <515478826@qq.com>
2019-06-01 22:02:37 +0800

    暂存

--- src/com/lcs/Test/UtilTest.java
+++ src/com/lcs/Test/UtilTest.java
@@ -10,13 +10,20 @@ package com.lcs.Test;
 
 public class UtilTest {
 
+    /**
+     * 我们不考虑java的库本身给我们提供的方法
+     * 实际上cs之间传输的是二进制数组才对
+     * 可是二进制数组是十分难以阅读的
+     * 我认为还是得把它转化成string型的来处理
+     **/
     public static void main(String[] args) {
-        String s = HexConvert.stringToHex("11019019");
-        System.out.println(s);
+        byte[] s = HexConvert.hexStringToBytes("010100130013");
+        System.out.println(HexConvert.binaryToHexString(s));
         String hex = HexConvert.hexToString("010100130013");
         for (int i = 0; i < hex.length(); i++) {
-            System.out.print ((int) hex.charAt(i));
+            System.out.print(Integer.valueOf(hex.charAt(i)));
             System.out.print(" ");
+//            System.out.print(HexConvert.);
         }
     }
 }
