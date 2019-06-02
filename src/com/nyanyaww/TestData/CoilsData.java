e558aca5bce2453e81a8e91725746f6b02844f04
lcs <515478826@qq.com>
2019-06-02 13:55:41 +0800

    /..../

---
+++ src/com/nyanyaww/TestData/CoilsData.java
@@ -0,0 +1,31 @@
+package com.nyanyaww.TestData;
+
+import java.util.Random;
+
+/**
+ * @author nyanyaww
+ * @program modbus
+ * @description 线圈的模拟数据
+ * @create 2019-06-02 13:46
+ **/
+public class CoilsData {
+
+    private byte[] coilNo = new byte[2000];
+
+    public CoilsData() {
+        init();
+    }
+
+    public void init() {
+        Random random = new Random();
+        for (int i = 0; i < 2000; i++) {
+            coilNo[i] = (byte) (random.nextBoolean() ? 1 : 0);
+            System.out.println(coilNo[i]);
+        }
+
+    }
+
+    public static void main(String[] args){
+        CoilsData coilsData = new CoilsData();
+    }
+}
