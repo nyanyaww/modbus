70b0e096fb0348ef05ef946b890738eafec188d7
lcs <515478826@qq.com>
2019-06-02 14:08:49 +0800

    总之我们随机生成了一些数据

---
+++ src/com/nyanyaww/TestData/HoldingRegisterData.java
@@ -0,0 +1,35 @@
+package com.nyanyaww.TestData;
+
+import java.util.Random;
+
+/**
+ * @author nyanyaww
+ * @program modbus
+ * @description 模拟生成保持寄存器的值
+ * @create 2019-06-02 14:01
+ **/
+public class HoldingRegisterData {
+    private char[] holdingRegisterNo = new char[125];
+
+    public HoldingRegisterData() {
+        init();
+    }
+
+    public void init() {
+        Random random = new Random();
+        for (int i = 0; i < 125; i++) {
+            holdingRegisterNo[i] = (char) random.nextInt(65535);
+            System.out.println(Integer.valueOf(holdingRegisterNo[i]));
+        }
+
+    }
+
+    public char[] getHoldingRegisterNo() {
+        return holdingRegisterNo;
+    }
+
+    public static void main(String[] args){
+        HoldingRegisterData holdingRegisterData = new HoldingRegisterData();
+
+    }
+}
---
+++ src/com/nyanyaww/TestData/InputRegisterData.java
@@ -0,0 +1,34 @@
+package com.nyanyaww.TestData;
+
+import java.util.Random;
+
+/**
+ * @author nyanyaww
+ * @program modbus
+ * @description 模拟生成输入寄存器的值
+ * @create 2019-06-02 14:07
+ **/
+public class InputRegisterData {
+    private char[] inputRegister = new char[125];
+
+    public InputRegisterData() {
+        init();
+    }
+
+    public void init() {
+        Random random = new Random();
+        for (int i = 0; i < 125; i++) {
+            inputRegister[i] = (char) random.nextInt(65535);
+            System.out.println(Integer.valueOf(inputRegister[i]));
+        }
+    }
+
+    public char[] getInputRegister() {
+        return inputRegister;
+    }
+
+    public static void main(String[] args) {
+        InputRegisterData holdingRegisterData = new InputRegisterData();
+
+    }
+}
